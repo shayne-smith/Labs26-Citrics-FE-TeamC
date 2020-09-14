e9beb5e7cea78d7af26836f489126f60ecfa9a9f
seanmx96 <seanmx96@users.noreply.github.com>
2020-09-14 14:57:02 -0500

    Adjusted positioning of SVG's in hero image

--- src/components/common/Hero.js
+++ src/components/common/Hero.js
@@ -2,18 +2,23 @@ import React from "react";
 import styled from "styled-components";
 import { ReactComponent as MailLogo } from "../../assets/mail4.svg";
 import { ReactComponent as Driftly } from "../../assets/driftly.svg";
+import { ReactComponent as Tagline } from "../../assets/letsFindOurNextHome.svg";
 
 const Hero = () => {
   const Wrapper = styled.div`
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    flex-wrap: wrap;
     margin: 2rem 0;
   `;
 
   return (
     <Wrapper>
-      <Driftly />
-      <MailLogo />
+      <div class="hero">
+        <Driftly />
+        <MailLogo class="mail" />
+      </div>
+      <Tagline class="tagline" />
     </Wrapper>
   );
 };
