4bc1eb4d34ebe36e168de30e039417b7ce17536c
seanmx96 <seanmx96@users.noreply.github.com>
2020-09-24 15:13:07 -0500

    Create data visualization page. Connect page to landing page.

---
+++ src/components/pages/DataViz/DataVizContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RenderDataViz from "./RenderDataViz";
+
+function DataVizContainer() {
+  return (
+    <>
+      <RenderDataViz />
+    </>
+  );
+}
+
+export default DataVizContainer;
--- src/components/pages/ExampleDataViz/RenderDataViz.js
+++ src/components/pages/ExampleDataViz/RenderDataViz.js
@@ -1,7 +1,7 @@
 /*eslint no-unused-vars: 0 */
-import React, { useState, useEffect } from 'react';
-import Plot from 'react-plotly.js';
-import { getDSData } from '../../../api';
+import React, { useState, useEffect } from "react";
+import Plot from "react-plotly.js";
+import { getDSData } from "../../../api";
 
 const initialState = {
   data: [],
@@ -9,26 +9,26 @@ const initialState = {
   frames: [],
   config: {
     displaylogo: false,
-    displayModeBar: false,
-  },
+    displayModeBar: false
+  }
 };
 
 function DataViz(props) {
   const [data, setData] = useState(initialState);
   const [figure, setFigure] = useState(null);
 
-  useEffect(() => {
-    function fetchDSData() {
-      getDSData(props.url, props.authState)
-        .then(res => {
-          setData(res);
-        })
-        .catch(err => {
-          setData({ data: null, err });
-        });
-    }
-    fetchDSData();
-  }, [props.url, props.authState]);
+  // useEffect(() => {
+  //   function fetchDSData() {
+  //     getDSData(props.url, props.authState)
+  //       .then(res => {
+  //         setData(res);
+  //       })
+  //       .catch(err => {
+  //         setData({ data: null, err });
+  //       });
+  //   }
+  //   fetchDSData();
+  // }, [props.url, props.authState]);
 
   return (
     <Plot
