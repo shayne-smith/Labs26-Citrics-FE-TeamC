9fc9a8796a5299f7c650c62e6db5235c88bc4d8e
Guillermo Alfaro <galfarotolon@gmail.com>
2020-09-29 15:46:01 -0500

    added more weather data, and changed modal styling

--- src/components/common/CityCardModal.js
+++ src/components/common/CityCardModal.js
@@ -1,29 +1,7 @@
 import React, { useState } from "react";
 import { Modal, Button } from "react-bootstrap";
 
-import styled from "styled-components";
-
-const CardComparison = styled.div`
-  background-size: cover;
-  background-repeat: no-repeat;
-  color: whitesmoke;
-  width: 29%;
-  height: 250px;
-  min-width: 300px;
-  margin: 1rem;
-  border-radius: 3px;
-  flex-grow: 1;
-  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-
-  &:hover {
-    transform: scale(1.02);
-    transition-duration: 0.1s;
-  }
-
-  @media (max-width: 768px) {
-    flex-wrap: wrap;
-  }
-`;
+//import styled from "styled-components";
 
 function CityCardModal(props) {
   console.log("this is city data", props.cities);
@@ -37,24 +15,77 @@ function CityCardModal(props) {
       >
         <Modal.Header closeButton>
           <Modal.Title id="contained-modal-title-vcenter">
-            <div>
-              {props.cities.map(city => {
-                return city.location === props.cityName ? (
-                  <h3> {city.location}</h3>
-                ) : (
-                  false
-                );
-              })}
-            </div>
+            {props.cities.map(city => {
+              return city.location === props.cityName ? (
+                <div className="modalHeader"> {city.location}</div>
+              ) : (
+                false
+              );
+            })}
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h4>Centered Modal</h4>
-          <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-            ac consectetur ac, vestibulum at eros.
-          </p>
+          <div className="modalSubheader">City Guide</div>
+          {props.cities.map(city => {
+            return city.location === props.cityName ? (
+              <div className="modalContainer">
+                <div className="modalData">
+                  <span className="modalKey">Average Temp in F </span>
+                  {city.FeelsLikeF}
+                </div>
+                <div className="modalData">
+                  <span className="modalKey">Average Temp in C </span>
+                  {city.FeelsLikeC}
+                </div>
+                <div className="modalData">
+                  <span className="modalKey">Heat Index </span>
+                  {city.HeatIndexC} C
+                </div>
+                <div className="modalData">
+                  <span className="modalKey">Wind Chill </span>
+                  {city.WindChillC} C
+                </div>
+                <div className="modalData">
+                  <span className="modalKey"> Dew Point </span>
+                  {city.DewPointC} C{" "}
+                </div>
+                <div className="modalData">
+                  <span className="modalKey"> Wind Gust </span>
+                  {city.WindGustKmph} km/hr{" "}
+                </div>
+                <div className="modalData">
+                  <span className="modalKey"> Cloud Cover </span>
+                  {city.cloudcover}{" "}
+                </div>
+                <div className="modalData">
+                  <span className="modalKey"> Humidity </span>
+                  {city.humidity}%
+                </div>
+
+                {/* 
+
+WindGustKmph: "15.50109649122807"
+cloudcover: "69.24890350877193"
+humidity: "87.8344298245614"
+id: 1
+image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Parque_Eagle_River%2C_Anchorage%2C_Alaska%2C_Estados_Unidos%2C_2017-09-01%2C_DD_02.jpg/2880px-Parque_Eagle_River%2C_Anchorage%2C_Alaska%2C_Estados_Unidos%2C_2017-09-01%2C_DD_02.jpg"
+location: "Juneau, AK"
+location_id: 1
+maxtempC: "-1.3793859649122806"
+maxtempF: "29.48201754385964"
+mintempC: "-5.640350877192983"
+mintempF: "21.81228070175439"
+precipMM: "8.517434210526318"
+pressure: "1010.9309210526316"
+totalSnow_cm: "3.773684210526315"
+uvIndex: "1.3475877192982457"
+visibility: "6.830043859649122"
+windspeedKmph: "9.12390350877193" */}
+              </div>
+            ) : (
+              false
+            );
+          })}
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={props.onHide}>Close</Button>
