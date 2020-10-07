63e9ab47c8977f8a9f2b440ac0051c9ee5d20168
Guillermo Alfaro <galfarotolon@gmail.com>
2020-10-06 20:10:52 -0500

    added more tabs, to be filled tomorrow

--- src/components/common/TestComponent.js
+++ src/components/common/TestComponent.js
@@ -12,51 +12,8 @@ export default class TestComponent extends React.Component {
   dataArray = [];
   cityNames = [];
   megaArray = [];
-  // states = Object.keys(this.state.weather[0]);
-
-  //   // //console.log(states);
-
-  //   // let obj = props.weather
-
-  //   // const stateKeys = Object.keys(obj)
-
-  //   // //console.log(keys)
-
-  //   // //values is city name and housing
-  //   // const valuesArr = Object.values(obj)
-
-  //   // //console.log(valuesArr)
-
-  //   // //all the cities followed by a comma and the state code
-  //   //let cityStateArr = []
-
-  //   // for (let i = 0; i < valuesArr.length; i++) {
-  //   //   let obj3 = Object.keys(valuesArr[i])
-  //   //   obj3.map(e => {
-  //   //     cityStateArr.push(e)
-  //   //   })
-  //   // }
-
-  //   //console.log(cityStateArr)
-
-  //   //   for (let i = 0; i < states.length; i++) {
-  //   //     for (let j = 0; j < cityStateArr.length; j++) {
-  //   //       const strReplace = states[i].replace(/"/g, "");
-  //   //       console.log('this data', weatherArray[0][strReplace][cityStateArr[j]]);
-  //   //     }
-  //   //   }
-  //   // })
 
   componentDidMount() {
-    // console.log(this.state.weather);
-    // console.log(this.stateCode);
-    //console.log(this.allCitiesArray);
-
-    //for each object in the array,
-    //get into the object, get all the object's keys
-    // if those keys match the name of the card
-    // render the data of those inner objects
-
     this.allCitiesArray.map(cities => {
       // console.log('this is cities', cities);
       // //cities keys has each city name
@@ -403,99 +360,11 @@ export default class TestComponent extends React.Component {
                     });
                   })}
                 </Tab>
+                <Tab eventKey="housing" title="Housing"></Tab>
+                <Tab eventKey="jobs" title="Jobs"></Tab>
+                <Tab eventKey="covid" title="Covid"></Tab>
+                <Tab eventKey="overview" title="Overview"></Tab>
               </Tabs>
-
-              {/*                                 
-                                {this.props.cities.map(city => {
-                                    return city.location === this.props.cityName ? (
-                                        <div className="modalContainer">
-                                            <div className="modal-leftSide">
-                                                <div className="modalData">
-                                                    <span className="modalKey">Average Temp (F)</span>
-                                                    {Number.parseFloat(city.FeelsLikeF).toFixed(2)}°F
-                              </div>
-                                                <div className="modalData">
-                                                    <span className="modalKey">Average Temp (C)</span>
-                                                    {Number.parseFloat(city.FeelsLikeC).toFixed(2)}°C
-                              </div>
-                                                <div className="modalData">
-                                                    <span className="modalKey">Heat Index</span>
-                                                    {Number.parseFloat(city.HeatIndexC).toFixed(2)}°C
-                              </div>
-                                                <div className="modalData">
-                                                    <span className="modalKey">Wind Chill</span>
-                                                    {Number.parseFloat(city.WindChillC).toFixed(2)}°C
-                              </div>
-                                                <div className="modalData">
-                                                    <span className="modalKey"> Dew Point</span>
-                                                    {Number.parseFloat(city.DewPointC).toFixed(2)}°C
-                              </div>
-                                                <div className="modalData">
-                                                    <span className="modalKey"> Wind Gust</span>
-                                                    {Number.parseFloat(city.WindGustKmph).toFixed(2)}{" "}
-                                km/hr
-                              </div>
-                                                <div className="modalData">
-                                                    <span className="modalKey"> Cloud Cover</span>
-                                                    {Number.parseFloat(city.cloudcover).toFixed(2)}%
-                              </div>
-                                                <div className="modalData">
-                                                    <span className="modalKey"> Humidity</span>
-                                                    {Number.parseFloat(city.humidity).toFixed(2)}%
-                              </div>
-                                                <div className="modalData">
-                                                    <span className="modalKey">Max Temp (C)</span>
-                                                    {Number.parseFloat(city.maxtempC).toFixed(2)}°C
-                              </div>
-                                            </div>
-                                            <div className="modal-rightSide">
-                                                <div className="modalData">
-                                                    <span className="modalKey">Max Temp (F)</span>
-                                                    {Number.parseFloat(city.maxtempF).toFixed(2)}°F
-                              </div>
-                                                <div className="modalData">
-                                                    <span className="modalKey">Min Temp (C)</span>
-                                                    {Number.parseFloat(city.mintempC).toFixed(2)}°C
-                              </div>
-                                                <div className="modalData">
-                                                    <span className="modalKey">Min Temp (F)</span>
-                                                    {Number.parseFloat(city.mintempF).toFixed(2)}°F
-                              </div>
-                                                <div className="modalData">
-                                                    <span className="modalKey">Precipitation (mm)</span>
-                                                    {Number.parseFloat(city.precipMM).toFixed(2)}
-                                                </div>
-                                                <div className="modalData">
-                                                    <span className="modalKey">Pressure</span>
-                                                    {Number.parseFloat(city.pressure).toFixed(2)}
-                                                </div>
-                                                <div className="modalData">
-                                                    <span className="modalKey">Total Snow (cm)</span>
-                                                    {Number.parseFloat(city.totalSnow_cm).toFixed(2)}
-                                                </div>
-                                                <div className="modalData">
-                                                    <span className="modalKey">UV Index</span>
-                                                    {Number.parseFloat(city.uvIndex).toFixed(2)}
-                                                </div>
-                                                <div className="modalData">
-                                                    <span className="modalKey">Visibility</span>
-                                                    {Number.parseFloat(city.visibility).toFixed(2)}
-                                                </div>
-                                                <div className="modalData">
-                                                    <span className="modalKey">Wind Speed</span>
-                                                    {Number.parseFloat(city.windspeedKmph).toFixed(
-                                                        2
-                                                    )}{" "}
-                                km/hr
-                              </div>
-                                            </div>
-                                        </div>
-                                    ) : (
-                                            false
-                                        );
-                                })} */}
-              {/* </Tab>
-                        </Tabs> */}
             </Modal.Body>
             <Modal.Footer>
               <Button className="modalBtn" onClick={this.props.onHide}>
