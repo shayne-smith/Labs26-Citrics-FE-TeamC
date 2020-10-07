15cd8c138fbf35cd2cf76f81f3064b0d155e6e1c
Guillermo Alfaro <galfarotolon@gmail.com>
2020-10-06 19:08:25 -0500

    switched weather data to render from the DS API directly, working on adding sub tabs for winter and summer

--- src/components/common/TestComponent.js
+++ src/components/common/TestComponent.js
@@ -9,7 +9,9 @@ export default class TestComponent extends React.Component {
 
   stateCode = Object.keys(this.state.weather[0]);
   allCitiesArray = Object.values(this.state.weather[0]);
-
+  dataArray = [];
+  cityNames = [];
+  megaArray = [];
   // states = Object.keys(this.state.weather[0]);
 
   //   // //console.log(states);
@@ -45,14 +47,40 @@ export default class TestComponent extends React.Component {
   //   //   }
   //   // })
 
-  componentDidUpdate() {
-    console.log(this.state.weather);
-    console.log(this.stateCode);
-    console.log(this.allCitiesArray);
+  componentDidMount() {
+    // console.log(this.state.weather);
+    // console.log(this.stateCode);
+    //console.log(this.allCitiesArray);
+
+    //for each object in the array,
+    //get into the object, get all the object's keys
+    // if those keys match the name of the card
+    // render the data of those inner objects
 
     this.allCitiesArray.map(cities => {
-      console.log(cities);
+      // console.log('this is cities', cities);
+      // //cities keys has each city name
+      //console.log('this is cities keys', Object.keys(cities));
+      this.cityNames.push(Object.keys(cities));
+      // console.log('this is cities values', Object.values(cities)[0]);
+      this.dataArray.push(Object.values(cities));
+
+      // return Object.keys(cities) === this.props.cityName ? (
+      //   <div>
+
+      //   </div>
+      // )
     });
+
+    //console.log('pushed', this.dataArray)
+
+    this.cityNames.map(array => {
+      array.map(cityName => {
+        this.megaArray.push(cityName);
+      });
+    });
+    //each individual city name in an array
+    //console.log('city names', this.megaArray)
   }
 
   render() {
@@ -74,9 +102,10 @@ export default class TestComponent extends React.Component {
               }}
             >
               <Modal.Title id="contained-modal-title-vcenter">
-                {this.props.cities.map(city => {
-                  return city.location === this.props.cityName ? (
-                    <div> {city.location}</div>
+                {this.megaArray.map(city => {
+                  //remove this ternary when all cities get added to the DB
+                  return city === this.props.cityName ? (
+                    <div>{city}</div>
                   ) : (
                     false
                   );
@@ -85,12 +114,149 @@ export default class TestComponent extends React.Component {
             </Modal.Header>
             <Modal.Body>
               <div className="modalSubheader">City Guide</div>
-              {/* <Tabs defaultActiveKey="weather" id="modal-tabs">
-                            <Tab eventKey="weather" title="Weather"> */}
+              <Tabs defaultActiveKey="weather" id="modal-tabs">
+                <Tab eventKey="weather" title="Weather">
+                  {this.allCitiesArray.map(cities => {
+                    let entries = Object.entries(cities);
 
-              {/* {this.state.weather.map(city => {
-                            console.log(city)
-                        })} */}
+                    return entries.map(entry => {
+                      return entry[0] === this.props.cityName ? (
+                        <div className="modalContainer">
+                          <div className="modal-leftSide">
+                            <div className="modalData">
+                              <span className="modalKey">Average Temp (F)</span>
+                              {Number.parseFloat(
+                                entry[1].summer.MaxTempF
+                              ).toFixed(2)}
+                              °F
+                            </div>
+                            <div className="modalData">
+                              <span className="modalKey">Average Temp (C)</span>
+                              {Number.parseFloat(
+                                entry[1].summer.FeelsLikeC
+                              ).toFixed(2)}
+                              °C
+                            </div>
+                            <div className="modalData">
+                              <span className="modalKey">Heat Index</span>
+                              {Number.parseFloat(
+                                entry[1].summer.HeatIndexC
+                              ).toFixed(2)}
+                              °C
+                            </div>
+                            <div className="modalData">
+                              <span className="modalKey">Wind Chill</span>
+                              {Number.parseFloat(
+                                entry[1].summer.WindChillC
+                              ).toFixed(2)}
+                              °C
+                            </div>
+                            <div className="modalData">
+                              <span className="modalKey"> Dew Point</span>
+                              {Number.parseFloat(
+                                entry[1].summer.DewPointC
+                              ).toFixed(2)}
+                              °C
+                            </div>
+                            <div className="modalData">
+                              <span className="modalKey"> Wind Gust</span>
+                              {Number.parseFloat(
+                                entry[1].summer.WindGust_kmph
+                              ).toFixed(2)}{" "}
+                              km/hr
+                            </div>
+                            <div className="modalData">
+                              <span className="modalKey"> Cloud Cover</span>
+                              {Number.parseFloat(
+                                entry[1].summer.CloudCover
+                              ).toFixed(2)}
+                              %
+                            </div>
+                            <div className="modalData">
+                              <span className="modalKey"> Humidity</span>
+                              {Number.parseFloat(
+                                entry[1].summer.Humidity
+                              ).toFixed(2)}
+                              %
+                            </div>
+                            <div className="modalData">
+                              <span className="modalKey">Max Temp (C)</span>
+                              {Number.parseFloat(
+                                entry[1].summer.MaxTempC
+                              ).toFixed(2)}
+                              °C
+                            </div>
+                          </div>
+                          <div className="modal-rightSide">
+                            <div className="modalData">
+                              <span className="modalKey">Max Temp (F)</span>
+                              {Number.parseFloat(
+                                entry[1].summer.MaxTempF
+                              ).toFixed(2)}
+                              °F
+                            </div>
+                            <div className="modalData">
+                              <span className="modalKey">Min Temp (C)</span>
+                              {Number.parseFloat(
+                                entry[1].summer.MinTempC
+                              ).toFixed(2)}
+                              °C
+                            </div>
+                            <div className="modalData">
+                              <span className="modalKey">Min Temp (F)</span>
+                              {Number.parseFloat(
+                                entry[1].summer.MinTempF
+                              ).toFixed(2)}
+                              °F
+                            </div>
+                            <div className="modalData">
+                              <span className="modalKey">
+                                Precipitation (mm)
+                              </span>
+                              {Number.parseFloat(
+                                entry[1].summer.Precip_mm
+                              ).toFixed(2)}
+                            </div>
+                            <div className="modalData">
+                              <span className="modalKey">Pressure</span>
+                              {Number.parseFloat(
+                                entry[1].summer.Pressure
+                              ).toFixed(2)}
+                            </div>
+                            <div className="modalData">
+                              <span className="modalKey">Total Snow (cm)</span>
+                              {Number.parseFloat(
+                                entry[1].summer.TotalSnow_cm
+                              ).toFixed(2)}
+                            </div>
+                            <div className="modalData">
+                              <span className="modalKey">UV Index</span>
+                              {Number.parseFloat(
+                                entry[1].summer.UVindex
+                              ).toFixed(2)}
+                            </div>
+                            <div className="modalData">
+                              <span className="modalKey">Visibility</span>
+                              {Number.parseFloat(
+                                entry[1].summer.Visibility
+                              ).toFixed(2)}
+                            </div>
+                            <div className="modalData">
+                              <span className="modalKey">Wind Speed</span>
+                              {Number.parseFloat(
+                                entry[1].summer.WindSpeed_kmph
+                              ).toFixed(2)}{" "}
+                              km/hr
+                            </div>
+                          </div>
+                        </div>
+                      ) : (
+                        false
+                      );
+                    });
+                  })}
+                </Tab>
+              </Tabs>
 
               {/*                                 
                                 {this.props.cities.map(city => {
